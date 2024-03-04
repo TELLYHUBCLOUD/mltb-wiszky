eec0655198f97e213bd6fb28d9647d72f29bd1f8
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-03-04 15:08:46 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,12 +37,12 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/TELLYHUBCLOUD/mltb-wiszky"
+    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'h-code'
+    UPSTREAM_BRANCH = 'master'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
